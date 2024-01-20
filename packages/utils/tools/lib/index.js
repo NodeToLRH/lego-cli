9e14a72c4e8e2d463ba00f5513f02cd531afec88
lrh21g <lingronghai@foxmail.com>
2024-01-20 14:01:40 +0800

    feat(utils): 新增 spinnerStart、sleep、exec、execAsync 工具函数

--- packages/utils/tools/lib/index.js
+++ packages/utils/tools/lib/index.js
@@ -1,6 +1,7 @@
 'use strict'
 
 const path = require('node:path')
+const process = require('node:process')
 
 function formatPath(p) {
   if (p && typeof p === 'string') {
@@ -19,7 +20,44 @@ function isObject(o) {
   return Object.prototype.toString.call(o) === '[object Object]'
 }
 
+function spinnerStart(msg, spinnerString = '|/-\\') {
+  const Spinner = require('cli-spinner').Spinner
+  const spinner = new Spinner(`${msg} %s`)
+  spinner.setSpinnerString(spinnerString)
+  spinner.start()
+  return spinner
+}
+
+function sleep(timeout = 1000) {
+  return new Promise(resolve => setTimeout(resolve, timeout))
+}
+
+function exec(command, args, options) {
+  const win32 = process.platform === 'win32'
+
+  const cmd = win32 ? 'cmd' : command
+  const cmdArgs = win32 ? ['/c'].concat(command, args) : args
+
+  return require('node:child_process').spawn(cmd, cmdArgs, options || {})
+}
+
+function execAsync(command, args, options) {
+  return new Promise((resolve, reject) => {
+    const p = exec(command, args, options)
+    p.on('error', (e) => {
+      reject(e)
+    })
+    p.on('exit', (c) => {
+      resolve(c)
+    })
+  })
+}
+
 module.exports = {
   formatPath,
   isObject,
+  spinnerStart,
+  sleep,
+  exec,
+  execAsync,
 }
