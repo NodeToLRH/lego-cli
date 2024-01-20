3179e6a7d9f05e3495d5bef5c93e7531ce0a82d2
lrh21g <lingronghai@foxmail.com>
2024-01-20 17:10:08 +0800

    chore: 更新 eslint 配置文件

--- eslint.config.js
+++ eslint.config.js
@@ -2,6 +2,11 @@ const antfu = require('@antfu/eslint-config').default
 
 module.exports = antfu(
   {
+    ignores: [
+      'packages/commands/init/template/**/**',
+    ],
+  },
+  {
     rules: {
       'no-console': 'off',
     },
